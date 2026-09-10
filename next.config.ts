65493f98597415ad45687b873799a0d381df506d
SANJANA K BHATIA <138809888+SANJANAKB01@users.noreply.github.com>
2026-09-10 14:14:59 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,23 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   }
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  }
+  serverExternalPackages: [
+    'firebase-admin',
+    'jwks-rsa',
+    'jose',
+  ],
 };
 
 export default nextConfig;
