7faea276724c95901297b7769f935ed59cd6401c
SANJANA K BHATIA <138809888+SANJANAKB01@users.noreply.github.com>
2026-09-10 14:58:55 +0530

    Refactor database import and model initialization
    Replaced db import with getAdminDb and updated model usage.

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -1,7 +1,8 @@
 import { generateText } from "ai";
 import { google } from "@ai-sdk/google"
 import { getRandomInterviewCover } from "@/lib/utils";
-import { db } from "@/firebase/admin";
+// import { db } from "@/firebase/admin";
+import { getAdminDb } from "@/firebase/admin";
 import { groq } from "@ai-sdk/groq";  // ← change
 
 export async function GET() {
@@ -12,6 +13,7 @@ export async function POST(request: Request) {
     const { type, role, level, techstack, amount, userid } = await request.json();
 
     try {
+        const db = getAdminDb();
         const { text: questions } = await generateText({
             // model: google('gemini-2.0-flash-lite'),
             // model: google('gemini-1.5-flash'),
